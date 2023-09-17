1f4a16834c7347084a7b5ddc81c41323011abb40
Febi Ago Jonata <febiago7@gmail.com>
2023-09-17 21:53:07 +0700

    update

--- js/header.js
+++ js/header.js
@@ -2,13 +2,13 @@ console.log("############################################");
 console.log("#        Hi there technical person         #");
 console.log("############################################");
 
-const _NETWORK_ID = 56;
+const _NETWORK_ID = 137;
 let _CONTRACT_DATA = {};
 
 const _API_URL = 'https://7yjhmmd390.execute-api.ap-south-1.amazonaws.com/prod'
 
 _CONTRACT_DATA[_NETWORK_ID] = {
-    network_name: "BSC Mainnet",
+    network_name: "Polygon Mainnet",
     explorer_url: 'https://bscscan.com/',
     STACKING: {
         sevenDays: {
@@ -46,7 +46,7 @@ let isMetamaskConnected;
 
 let selectedAccount;
 
-let web3Main = new Web3('https://bsc-dataseed2.defibit.io');
+let web3Main = new Web3('https://rpc.ankr.com/polygon');
 
 // Create an instance of Notyf
 var notyf = new Notyf({
